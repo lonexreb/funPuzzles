d1522a2e71015941d40197e9f0929172c0eab716
lonexreb <95570942+lonexreb@users.noreply.github.com>
2022-08-06 14:06:47 +0530

    Create findGCD_searchMethod.c
    OUTPUT:
    Enter the value of num1: 10
    Enter the value of num2: 15
    The GCD of 10 and 15 is: 5 
    
    OUTPUT:
    Enter the value of num1: 10
    Enter the value of num2: 10
    The GCD of 10 and 10 is: 10

---
+++ findGCD_searchMethod.c
@@ -0,0 +1,53 @@
+# include <stdio.h>
+//# include <stdbool.h>
+
+int findSmaller (int num1, int num2)
+{
+    //TODO: Find the greater of the two numbers
+    if (num1 <= num2) return num1;
+    else return num2;
+}
+
+int findGCD (int num1, int num2)
+{
+    //TODO_1: If num1 == num2, return num1
+    //TODO_2: Run a loop (start from 2) to determine a number which divides both num1 and num2
+    //TODO_3: Store the dividing num as "gcd"
+    //TODO_4: Store num1 as num1/gdc and num2 as num2/gcd
+    //TODO_5: Progress with the loop, till it divides num1 and num2,
+    //Repeat from TODO_3 and store gcd as gcd * i 
+    //Else return gcd
+  
+    if (num1 == num2) return num1;
+    
+    int gcd = 1;
+    int smaller = findSmaller(num1, num2);
+    for (int i = 2; i <= smaller; ++i )
+    {
+
+        if ((num1 % i == 0) && (num2 % i == 0))
+        {
+            gcd = gcd * i;
+            num1 = num1 / i;
+            num2 = num2 / i;
+            smaller = findSmaller(num1, num2);
+            break;
+        }
+        
+    }
+    return gcd;
+}
+
+int main ()
+{
+    int num1, num2;
+
+    printf("Enter the value of num1: ");
+    scanf("%d", &num1);
+    printf("Enter the value of num2: ");
+    scanf("%d", &num2);
+
+    printf("The GCD of %d and %d is: %d ", num1, num2, findGCD(num1, num2));
+
+    return 0; 
+}
