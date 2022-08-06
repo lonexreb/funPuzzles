631a5de3f5d7510089177166aaaab36bc650b0a8
lonexreb <95570942+lonexreb@users.noreply.github.com>
2022-08-06 14:29:06 +0530

    Create findLCM_gcdMethod.c
    1. First, GCD was calculated by the search, divide, store, and update method (SDSU) - Proprietary License @Copyright Shubhankar Tripathy
    
    2. Multiply GCD with the number1/gcd, number2/gcd
    
    Further development ---> Generalized LCM for n number of arguments!

---
+++ findLCM_gcdMethod.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int findSmaller(int num1, int num2)
+{
+    if (num1 <= num2) return num1;
+    return num2;
+}
+
+int findGCD(int num1, int num2)
+{
+    if (num1 == num2) return num1;
+    int gcd = 1;
+    int smaller = findSmaller (num1, num2);
+
+    for (int i = 2; i <= smaller; ++i)
+    {
+        if((num1%i == 0) && (num2%i == 0))
+        {
+            gcd = gcd * i;
+            num1 = num1/ i;
+            num2 = num2 / i;
+            smaller = findSmaller(num1, num2);
+        }
+    }
+    return gcd;
+}
+
+int findLCM(int num1, int num2)
+{
+    int gcd = findGCD (num1, num2); 
+    num1 = num1 / gcd;
+    num2 = num2 / gcd;
+
+    int LCM = gcd * num1 * num2;
+
+    return LCM;
+
+}
+
+int main ()
+{
+    int num1, num2;
+
+    printf("Enter the value of num1: ");
+    scanf("%d", &num1);
+    printf("Enter the value of num2: ");
+    scanf("%d", &num2);
+
+    printf("The LCM of %d and %d is: %d ", num1, num2, findLCM(num1, num2));
+    
+    return 0;
+
+}
