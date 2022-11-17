5135a3569bf646bab00c9d0caf681141414c5440
lonexreb <95570942+lonexreb@users.noreply.github.com>
2022-11-16 20:08:48 -0500

    Update findGCD_searchMethod.c

--- findGCD_searchMethod.c
+++ findGCD_searchMethod.c
@@ -3,37 +3,33 @@
 
 int findSmaller (int num1, int num2)
 {
-    //TODO: Find the greater of the two numbers
+    // Find the greater of the two numbers
     if (num1 <= num2) return num1;
     else return num2;
 }
 
 int findGCD (int num1, int num2)
 {
-    //TODO_1: If num1 == num2, return num1
-    //TODO_2: Run a loop (start from 2) to determine a number which divides both num1 and num2
-    //TODO_3: Store the dividing num as "gcd"
-    //TODO_4: Store num1 as num1/gdc and num2 as num2/gcd
-    //TODO_5: Progress with the loop, till it divides num1 and num2,
-    //Repeat from TODO_3 and store gcd as gcd * i 
-    //Else return gcd
+    // If num1 == num2, return num1
+    // Run a loop (start from 2) to determine a number which divides both num1 and num2
+    // Store the dividing num as "gcd"
+    // Store num1 as num1/gdc and num2 as num2/gcd
+    // Progress with the loop, till it divides num1 and num2,
+    // Repeat from TODO_3 and store gcd as gcd * i 
+    // Else return gcd
   
     if (num1 == num2) return num1;
     
     int gcd = 1;
     int smaller = findSmaller(num1, num2);
-    for (int i = 2; i <= smaller; ++i )
-    {
-
-        if ((num1 % i == 0) && (num2 % i == 0))
-        {
-            gcd = gcd * i;
-            num1 = num1 / i;
-            num2 = num2 / i;
-            smaller = findSmaller(num1, num2);
-            break;
-        }
-        
+    for (int i = 2; i <= smaller; ++i ) {
+      if ((num1 % i == 0) && (num2 % i == 0)) {
+        gcd = gcd * i;
+        num1 = num1 / i;
+        num2 = num2 / i;
+        smaller = findSmaller(num1, num2);
+        break;
+        }        
     }
     return gcd;
 }
@@ -48,6 +44,5 @@ int main ()
     scanf("%d", &num2);
 
     printf("The GCD of %d and %d is: %d ", num1, num2, findGCD(num1, num2));
-
     return 0; 
 }
